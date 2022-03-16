14408fba324d68083ee47a45e289f5f316214e6c
tuncergurel <tuncergurel.tr@gmail.com>
2022-03-16 13:30:48 +0300

    started

---
+++ user/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render,redirect
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+# Create your views here.
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        newUser = User(username = username)
+        newUser.set_password(password)
+        newUser.save()
+        login(request,newUser)
+        messages.success(request,"Register Successfuly...")
+        return redirect("index")
+
+    context = {
+        "form": form
+    }
+    return render(request,"register.html",context)
+
+
+    # if request.method == "POST":
+    #     form = RegisterForm(request.POST)
+    #     if form.is_valid():
+    #         username = form.cleaned_data.get('username')
+    #         password = form.cleaned_data.get('password')
+    #
+    #         newUser = User(username = username)
+    #         newUser.set_password(password)
+    #         newUser.save()
+    #         login(request,newUser)
+    #         return redirect("index")
+    #
+    #     context = {
+    #         "form": form
+    #     }
+    #     return render(request,"register.html",context)
+            
+    # else:
+    #     form = RegisterForm()
+    #     context = {
+    #         "form": form
+    #     }
+    #     return render(request,"register.html",context)
+"""    form = RegisterForm()
+
+    context = {
+        "form":form
+    }
+    return render(request,"register.html",context)"""
+
+
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user is None:
+            messages.info(request,"Username or Password is Incorrect")
+            return render(request,"login.html",context)
+
+        messages.success(request,"Login Success")
+        login(request,user)
+        return redirect("index")
+
+    return render(request,"login.html",context)
+
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request,"Logout Success")
+    return redirect("index")
